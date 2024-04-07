e483140fcc0556742cb053a0aaa5016a5483c510
Mohammed Benlamlih <benlamlih.med@gmail.com>
2024-04-07 22:24:12 +0200

    Initial commit of microservices

---
+++ appointmentservice/src/test/java/net/benlamlih/appointmentservice/AppointmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.benlamlih.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ paymentservice/src/test/java/net/benlamlih/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.benlamlih.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
